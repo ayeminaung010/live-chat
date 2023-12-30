d1f79fcfdf65eb7d3fd580782dac46c19c9f258f
yasuoOwO <heinmina05@gmail.com>
2023-12-30 18:28:19 +0630

    firebase set up

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBnN8f0YVAGB0OwhEejONh8hFSw09j5NnA",
+    authDomain: "vue-blog-system-98af4.firebaseapp.com",
+    projectId: "vue-blog-system-98af4",
+    storageBucket: "vue-blog-system-98af4.appspot.com",
+    messagingSenderId: "502945559178",
+    appId: "1:502945559178:web:0f17daa2a0819a3cc4b373"
+  };
+
+//   init firebase
+const app = initializeApp(firebaseConfig);
+
+//  database setup
+const db=getFirestore(app);
+
+export { db };
