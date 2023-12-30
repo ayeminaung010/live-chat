5a800e8cfc5dd03326d6dbc1cedd40c40209e517
yasuoOwO <heinmina05@gmail.com>
2023-12-30 20:32:52 +0630

    feat: implement sign up & update profiile

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,6 +1,7 @@
 <template>
     <h2>Sign up </h2>
     <form @submit.prevent="signUpHandler">
+        <span class="error">{{ error }}</span>
         <input type="text" placeholder="Display Name" v-model="displayName">
         <input type="email" placeholder="Email" v-model="email">
         <input type="password" placeholder="Password" v-model="password">
@@ -10,17 +11,29 @@
 
 <script>
 import { ref } from 'vue';
+import {auth} from '../firebase/config'
+import { createUserWithEmailAndPassword,updateProfile  } from 'firebase/auth'
+
 export default {
     setup(){
         const displayName = ref("");
         const email = ref("");
         const password = ref("");
-        
-        const signUpHandler = () => {
-            console.log(displayName.value , email.value , password.value);
+        const error = ref(null);
+
+        const signUpHandler = async() => {
+            try{
+               const res = await createUserWithEmailAndPassword(auth,email.value,password.value);
+               if(!res){
+                throw new Error("Could not create user");
+               }
+               await updateProfile(res.user,{displayName:displayName.value})
+            }catch(err){
+                error.value  = err.message
+            }
         }
-        
-        return {displayName,email,password,signUpHandler};
+
+        return {displayName,email,password,signUpHandler,error};
     }
 }
 </script>
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,5 +17,6 @@ const app = initializeApp(firebaseConfig);
 
 //  database setup
 const db=getFirestore(app);
+const auth = getAuth(app);
+export { auth, db };
 
-export { db };
